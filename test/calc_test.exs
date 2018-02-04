b1efd0ca944fd2a338451e054de9a2523ea3b49f
David Celentano <davidwcelentano@gmail.com>
2018-02-04 12:34:04 -0500

    added tests

--- test/calc_test.exs
+++ test/calc_test.exs
@@ -2,7 +2,27 @@ defmodule CalcTest do
   use ExUnit.Case
   doctest Calc
 
-  test "greets the world" do
-    assert Calc.hello() == :world
+  test "addition" do
+    assert Calc.eval("9 + 9") == 18
+  end
+
+  test "subtraction" do
+    assert Calc.eval("9 - 9") == 0
+  end
+
+  test "multiplication" do
+    assert Calc.eval("9 * 9") == 81
+  end
+
+  test "division" do
+    assert Calc.eval("9 / 9") == 1
+  end
+
+  test "parens" do
+    assert Calc.eval("5 * (9 + 9)") == 90
+  end
+
+  test "bonus" do
+    assert Calc.eval("(5 + 4) - (6 * 6)") == -27
   end
 end
